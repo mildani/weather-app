5ce68851b4639979276ab57772c7105df1142c4a
mildani <60314861+mildani@users.noreply.github.com>
2020-02-02 19:47:51 +0100

    Added format date function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-//Display the weather conditions (and city) from the weather API response
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  let weekday = weekdays[date.getDay()];
+  let day = date.getDate();
+  let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let month = months[date.getMonth()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours<10) {
+    hours = `0${hours}`;
+  }
+  if (minutes <10) {
+    minutes = `0${minutes}`;
+  }
+return `${weekday}, ${day} ${month}, ${hours}:${minutes}`;
+}
 
 function showWeather(response) {
   document.querySelector("#city-display").innerHTML = response.data.name;
@@ -6,19 +23,10 @@ function showWeather(response) {
   document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#weather-description-display").innerHTML = response.data.weather[0].main;
+  document.querySelector("#time-stamp-display").innerHTML = formatDate(response.data.dt*1000);
 }
 
-//Display city name and receive temperature based on form input
 
-function overwriteCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#searched-city");
-  if (searchInput.value) {
-    let displayCity = document.querySelector("#city-display");
-    let city = searchInput.value;
-    displayCity.innerHTML = city;
-  }
-}
 
 function getTemperature(event) {
   event.preventDefault();
@@ -32,13 +40,7 @@ function getTemperature(event) {
   }
 }
 
-//Display city name and receive temperature based on current location
 
-function overwriteCurrentCity(response) {
-  let city = response.data.name;
-  let displayCity = document.querySelector("#city-display");
-  displayCity.innerHTML = city;
-}
 
 function getCurrentTemperature(position) {
   let latitude = position.coords.latitude;
@@ -57,7 +59,7 @@ function getCurrentLocation() {
 
 let form = document.querySelector("#search-city");
 form.addEventListener("submit", getTemperature);
-form.addEventListener("submit", overwriteCity);
+
 
 let button = document.querySelector("#button-current-location");
 button.addEventListener("click", getCurrentLocation);
